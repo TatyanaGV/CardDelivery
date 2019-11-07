481443961adb765f5c0192cd98ada8258aafc55f
TatyanaGV <gvtatyana@mail.ru>
2019-11-07 20:34:46 +0300

    Changed getDate. Added @DisplayName

--- src/test/java/CardDelivery.java
+++ src/test/java/CardDelivery.java
@@ -1,7 +1,10 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
+
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.withText;
@@ -9,11 +12,12 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class CardDelivery {
     @Test
+    @DisplayName("Positive test. All fields are filled")
     void shouldRegisterByDeliverCardPositive() {
         open("http://localhost:9999");
         $("[data-test-id=city] input").setValue("Москва");
         $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.CONTROL, "a"), Keys.DELETE);
-        $("[data-test-id=date] input").setValue(addDate( ));
+        $("[data-test-id=date] input").setValue(getDate(3));
         $("[data-test-id=name] input").setValue("Василий Васильев");
         $("[data-test-id=phone] input").setValue("+79200000000");
         $("[data-test-id=agreement]").click();
@@ -22,20 +26,21 @@ public class CardDelivery {
     }
 
     @Test
+    @DisplayName("Negative test. 'Mobile phone' is empty")
     void shouldRegisterByDeliverCardNegative() {
         open("http://localhost:9999");
         $("[data-test-id=city] input").setValue("Москва");
         $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.CONTROL, "a"), Keys.DELETE);
-        $("[data-test-id=date] input").setValue(addDate( ));
+        $("[data-test-id=date] input").setValue(getDate(3));
         $("[data-test-id=name] input").setValue("Василий Васильев");
         $("[data-test-id=agreement]").click();
         $$("button").find(exactText("Забронировать")).click();
         $(withText("Поле обязательно для заполнения")).waitUntil(visible, 15000);
     }
 
-    public String addDate () {
+    public String getDate(int plusDays) {
         LocalDate today = LocalDate.now();
-        LocalDate date3Day = today.plusDays(3);
+        LocalDate date3Day = today.plusDays(plusDays);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.uuuu");
         String formatDate = formatter.format(date3Day);
         return formatDate;
